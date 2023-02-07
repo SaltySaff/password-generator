8cbdf17a59a60da64aa694ae103cad7a072e97c7
saltysaff <d_ash12345@hotmail.co.uk>
2023-02-07 15:57:54 +0000

    main functionality complete

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let passwordEl = document.getElementById("password-el")
+let passwordOne = document.getElementById("password-one")
+let passwordTwo = document.getElementById("password-two")
 passwordLength = 15
 
 function randomChar() {
@@ -9,10 +10,16 @@ function randomChar() {
     return characters[randomIndex]
 }
 
-function generatePassword() {
+function generatePasswords() {
     for (i = 0; i < passwordLength; i++) {
-        passwordEl.textContent += randomChar()
+        passwordOne.textContent += randomChar()
+        passwordTwo.textContent += randomChar()
     }
 }
 
-console.log(generatePassword())
+function processPasswords() {
+    passwordOne.textContent = ""
+    passwordTwo.textContent = ""
+    generatePasswords()
+}
+
